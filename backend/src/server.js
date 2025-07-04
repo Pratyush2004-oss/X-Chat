005a9aa29179f3d597d03301f88837ec027f08b2
Pratyush2004-oss <pratyush@sitp.ac.in>
2025-07-04 08:48:13 +0530

    Vercel config

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,12 +13,10 @@ import NotificationRoutes from './routes/notifications.route.js';
 
 // importing arcjet middleware for security purposes
 import { arcjetMiddleware } from './middleware/arcjet.middleware.js';
-import job from './config/cron.js';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-job.start();
 
 app.use(clerkMiddleware());
 app.use(arcjetMiddleware);
@@ -41,12 +39,18 @@ app.use((err, req, res, next) => {
 const startServer = async () => {
     try {
         await connectDB();
-        app.listen(ENV.PORT, () => {
-            console.log(`Server listening on port ${ENV.PORT}`);
-        })
+        // listen to local development
+        if (ENV.NODE_ENV !== 'production') {
+            app.listen(ENV.PORT, () => {
+                console.log(`Server listening on port ${ENV.PORT}`);
+            })
+        }
     } catch (error) {
         console.log("Failed to start Server:" + error.message);
         process.exit(1);
     }
 }
 startServer();
+
+// export for vercel
+export default app;
